4f37831627ebc4ae551cff75eb6d73d3a4c11050
Esraa Khaled <130283219+EsraaKhaled-PaySky@users.noreply.github.com>
2025-02-26 14:55:11 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,10 @@ If an invalid subreddit is given, the function should return 0
 import requests
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit."""
+    """Return the number of subscribers for a given subreddit.
+
+    If the subreddit is invalid or an error occurs, return 0.
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "MyRedditBot/1.0"}
 
@@ -17,7 +20,6 @@ def number_of_subscribers(subreddit):
         if response.status_code == 200:
             data = response.json()
             return data.get("data", {}).get("subscribers", 0)
-        else:
-            return 0
+        return 0
     except requests.RequestException:
         return 0
